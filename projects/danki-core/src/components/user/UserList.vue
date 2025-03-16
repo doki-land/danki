8156b207307d3f316859b89e7988aa32087555d1
Aster <galaster@foxmail.com>
2025-03-17 03:24:14 +0800

    Reexport components

---
+++ projects/danki-core/src/components/user/UserList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { User } from '../../index'
+import { UserInfo } from './UserInfo.vue'
+
+defineProps<{
+  users: User[]
+}>()
+</script>
+
+<template>
+  <div class="user-list">
+    <div v-for="user in users" :key="user.id" class="user-item">
+      <UserInfo :user="user" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.user-list {
+  .user-item {
+    margin-bottom: 1rem;
+  }
+}
+</style>
---
+++ projects/danki-demo/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// 这里可以导入组件
+</script>
+
+<template>
+  <div class="app">
+    <h1>Welcome to Danki Demo</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app {
+  padding: 2rem;
+  text-align: center;
+}
+</style>
