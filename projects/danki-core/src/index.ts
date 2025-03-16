eafcc6bf2fc2eddc4da40d6596c6ea2b185409c6
Aster <galaster@foxmail.com>
2025-03-17 05:01:22 +0800

    Fix workspace config

--- projects/danki-core/src/index.ts
+++ projects/danki-core/src/index.ts
@@ -1,11 +1,8 @@
 // 导出createDanki函数
-export { createDanki } from './createDanki'
+export {createDanki} from './createDanki'
 
 // 导出组件
 export * from './components'
 
 // 导出类型定义
 export * from './types'
-
-// 导出版本号
-export const version = '0.0.1'
